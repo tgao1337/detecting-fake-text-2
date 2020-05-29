03c9b24a61fb54ff100179c4b9e2575973b3f02d
Tommy Gao <gaotommy10@gmail.com>
2020-05-29 13:37:56 -0400

    Removed special characters ' " - and use long text

--- text_analyze_test.py
+++ text_analyze_test.py
@@ -20,36 +20,36 @@ from backend import api
 raw_text = """
     In a shocking finding, scientist discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.
 
-    The scientist named the population, after their distinctive horn, Ovid’s Unicorn. These four-horned, silver-white unicorns were previously unknown to science.
+    The scientist named the population, after their distinctive horn, Ovid's Unicorn. These four-horned, silver-white unicorns were previously unknown to science.
 
     Now, after almost two centuries, the mystery of what sparked this odd phenomenon is finally solved.
 
     Dr. Jorge Pérez, an evolutionary biologist from the University of La Paz, and several companions, were exploring the Andes Mountains when they found a small valley, with no other animals or humans. Pérez noticed that the valley had what appeared to be a natural fountain, surrounded by two peaks of rock and silver snow.
 
-    Pérez and the others then ventured further into the valley. “By the time we reached the top of one peak, the water looked blue, with some crystals on top,” said Pérez.
+    Pérez and the others then ventured further into the valley. "By the time we reached the top of one peak, the water looked blue, with some crystals on top," said Pérez.
 
-    Pérez and his friends were astonished to see the unicorn herd. These creatures could be seen from the air without having to move too much to see them – they were so close they could touch their horns.
+    Pérez and his friends were astonished to see the unicorn herd. These creatures could be seen from the air without having to move too much to see them - they were so close they could touch their horns.
 
-    While examining these bizarre creatures the scientists discovered that the creatures also spoke some fairly regular English. Pérez stated, “We can see, for example, that they have a common ‘language,’ something like a dialect or dialectic.”
+    While examining these bizarre creatures the scientists discovered that the creatures also spoke some fairly regular English. Pérez stated, "We can see, for example, that they have a common 'language,' something like a dialect or dialectic."
 
     Dr. Pérez believes that the unicorns may have originated in Argentina, where the animals were believed to be descendants of a lost race of people who lived there before the arrival of humans in those parts of South America.
 
-    While their origins are still unclear, some believe that perhaps the creatures were created when a human and a unicorn met each other in a time before human civilization. According to Pérez, “In South America, such incidents seem to be quite common.”
+    While their origins are still unclear, some believe that perhaps the creatures were created when a human and a unicorn met each other in a time before human civilization. According to Pérez, "In South America, such incidents seem to be quite common."
 
-    However, Pérez also pointed out that it is likely that the only way of knowing for sure if unicorns are indeed the descendants of a lost alien race is through DNA. “But they seem to be able to communicate in English quite well, which I believe is a sign of evolution, or at least a change in social organization,” said the scientist.
+    However, Pérez also pointed out that it is likely that the only way of knowing for sure if unicorns are indeed the descendants of a lost alien race is through DNA. "But they seem to be able to communicate in English quite well, which I believe is a sign of evolution, or at least a change in social organization," said the scientist.
     """
-raw_text = """
-    In a shocking finding, scientist discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.
-    """
+# raw_text = """
+#     In a shocking finding, scientist discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.
+#     """
 
 '''
 Tests for BERT
 '''
-lm = api.BERTLM()
+"""lm = api.BERTLM()
 start = api.time.time()
-payload = lm.check_probabilities(raw_text, topk=5)
+payload_bert = lm.check_probabilities(raw_text, topk=5)
 end = api.time.time()
-print("{:.2f} Seconds for a run with BERT".format(end - start))
+print("{:.2f} Seconds for a run with BERT".format(end - start))"""
 # print("SAMPLE:", sample)
 
 '''
@@ -57,22 +57,26 @@ Tests for GPT-2
 '''
 lm = api.LM()
 start = api.time.time()
-payload_bert = lm.check_probabilities(raw_text, topk=5)
+payload = lm.check_probabilities(raw_text, topk=5)
 end = api.time.time()
 print("{:.2f} Seconds for a check with GPT-2".format(end - start))
+'''for item in payload["pred_topk"]:
+  print(lm.postprocess(item[0][0]))'''
 
-start = api.time.time()
+
+
+"""start = api.time.time()
 sample = lm.sample_unconditional()
 end = api.time.time()
-print("{:.2f} Seconds for a sample from GPT-2".format(end - start))
+print("{:.2f} Seconds for a sample from GPT-2".format(end - start))"""
 # print("SAMPLE:", sample)
 
 res = {
         "request": {'project': "new", 'text': raw_text},
-        "result": payload_bert
+        "result": payload
     }
 
 print(res)
 
-with open('test_json2.json', 'w') as outfile:
-    json.dump(res, outfile)
+#with open('test_json2.json', 'w') as outfile:
+#    json.dump(res, outfile)
